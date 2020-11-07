e4d357a5c1d3005ac9e48ed8927c576cd20f9be5
paymon <paymon.jalali@gmail.com>
2020-11-07 11:55:41 -0800

    webrtc

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,6 +1,5 @@
 function startup() {
     const video = document.querySelector('video');
-
     navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
     .then((stream) => {video.srcObject = stream});
 }
