62039cc6e48ca77119323bb08ad09394cbd860d5
paymon <paymon.jalali@gmail.com>
2020-11-07 12:33:39 -0800

    adding taking photos

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,16 +1,31 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const takePhotoButton = document.getElementById("takePhotoButton");
+const retakePhotoButton = document.getElementById("retakePhotoButton");
+const evaluate = document.getElementById("evaluate");
+
 function startup() {
-    const img = document.querySelector('#screenshot img');
-    const video = document.querySelector('video');
     navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
     .then((stream) => {video.srcObject = stream});
 }
 
 function takePhoto() {
-    const video = document.getElementById("video");
-    const canvas = document.getElementById("canvas");
-
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
     canvas.getContext('2d').drawImage(video, 0, 0);
+
+    video.style.display = "none";
+    takePhotoButton.style.display = "none";
+    canvas.style.display = "block";
+    retakePhotoButton.style.display = "block";
+    evaluate.style.display = "block";
+}
+
+function retakePhoto() {
+    video.style.display = "block";
+    takePhotoButton.style.display = "block";
+    canvas.style.display = "none";
+    retakePhotoButton.style.display = "none";
+    evaluate.style.display = "none";
 }
