cf24666854382e7c71615a3a6c476aa363741d01
paymon <paymon.jalali@gmail.com>
2020-11-07 11:53:57 -0800

    back

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,8 +1,6 @@
-var gum = mode => 
-  navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
-  .then(stream => (video.srcObject = stream))
-  .catch(e => log(e));
+function startup() {
+    const video = document.querySelector('video');
 
-var stop = () => video.srcObject && video.srcObject.getTracks().map(t => t.stop());
-
-var log = msg => div.innerHTML += "<p>" + msg + "</p>";
+    navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
+    .then((stream) => {video.srcObject = stream});
+}
