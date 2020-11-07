3731c5e3b7341578cdcddf75c49c8af5a02e650e
paymon <paymon.jalali@gmail.com>
2020-11-07 15:39:42 -0800

    adding dynamic filename

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import base64
+import time
+
   
 app = Flask(__name__, template_folder='templates')
   
@@ -12,6 +14,7 @@ def index():
 def evaluate():
         imgDataB64 = request.form['imageData']
         imgDataPNG = base64.b64decode(imgDataB64[22:])
-        with open("original.png","wb") as fo:
+        fileName = str(time.time()) + ".png"
+        with open(fileName,"wb") as fo:
                 fo.write(imgDataPNG)
         return render_template('result.html', data=imgDataB64)
