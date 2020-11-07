5a2c2de6ed1c93d16384fa79bf28f37baa1e8bb4
paymon <paymon.jalali@gmail.com>
2020-11-07 12:15:23 -0800

    canvas test

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,5 +1,8 @@
-function startup() {
-    const video = document.querySelector('video');
-    navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
-    .then((stream) => {video.srcObject = stream});
+function takePhoto() {
+    const video = document.getElementById("video");
+    const canvas = document.getElementById("canvas");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
 }
