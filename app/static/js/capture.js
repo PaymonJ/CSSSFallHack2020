a81f2b6313a1b7d9c06549f1dc47d0861012dd19
paymon <paymon.jalali@gmail.com>
2020-11-07 12:26:19 -0800

    modular

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,9 +1,16 @@
+function startup() {
+    const img = document.querySelector('#screenshot img');
+    const video = document.querySelector('video');
+    navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment', aspectRatio: 4 / 3}})
+    .then((stream) => {video.srcObject = stream});
+}
+
 function takePhoto() {
     const video = document.getElementById("video");
     const canvas = document.getElementById("canvas");
 
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    
+
     canvas.getContext('2d').drawImage(video, 0, 0);
 }
