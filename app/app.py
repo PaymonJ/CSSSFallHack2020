06df41214f8944f5ab9a10171c01ce7c51743470
paymon <paymon.jalali@gmail.com>
2020-11-07 14:55:01 -0800

    pushing to github

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import base64
   
 app = Flask(__name__, template_folder='templates')
   
@@ -8,5 +9,9 @@ def index():
         return render_template('index.html')
 
 @app.route('/evaluate', methods = ['POST'])
-def evaluate(): 
+def evaluate():
+        imgDataB64 = request.form['imageData']
+        imgDataPNG = base64.b64decode(imgDataB64[22:])
+        with open("original.png","wb") as fo:
+                fo.write(imgDataPNG)
         return "<h1>Hi, Karan!</h1>"
