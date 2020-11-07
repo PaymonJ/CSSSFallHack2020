843d107af8d87071a0e5e7ab51383311a8917a28
paymon <paymon.jalali@gmail.com>
2020-11-07 11:35:01 -0800

    changing facing mode

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -1,9 +1,19 @@
+var front = false;
+
 function startup() {
     console.log("hello")
     const constraints = {
         video: {facingMode: {exact: "environment"}},
     };
 
+    const video = document.querySelector('video');
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {video.srcObject = stream});
+}
+
+function changeCamera() {
+    document.getElementById('flip-button').onclick = function() { front = !front; };
+    var constraints = { video: { facingMode: (front? "user" : "environment") } };
     const video = document.querySelector('video');
 
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {video.srcObject = stream});
